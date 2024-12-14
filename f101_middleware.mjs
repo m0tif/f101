1eb7d99fd8a66c74626c18e63ee84b85649663c3
vimwitch <jchancehud@gmail.com>
2024-12-13 19:57:48 -0800

    fix: call next in middleware

--- f101_middleware.mjs
+++ f101_middleware.mjs
@@ -1,10 +1,11 @@
-export default (req, res) => {
+export default (req, res, next) => {
   /// assign a mdn compliant URL object
   /// how we build it varies based on whether we're in NodeJS
   /// or a cloudflare datacenter
-  req.url = new URL(
+  req.mdn_url = new URL(
     process.env.WEBWORKER
       ? req.url
       : req.protocol + "://" + req.get("host") + req.originalUrl,
   );
+  next();
 };
